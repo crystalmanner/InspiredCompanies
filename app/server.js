ea6f8ca10e20bc3cf5b05ddd7a5c174d74cb7242
crystalmanner <crystalmanner130@gmail.com>
2020-06-22 03:05:12 -0400

    update port

--- app/server.js
+++ app/server.js
@@ -10,6 +10,7 @@ const app = express();
 // };
 
 app.use(cors());
+app.options('*', cors()); // include before other routes for preflight checks
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -23,7 +24,7 @@ app.get('*', (req, res) => {
 })
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
