965f9fcaf017a604c8e2ea4577d5c64c0ad03a25
crystalmanner <crystalmanner130@gmail.com>
2020-07-01 18:54:16 -0400

    fix issue

--- app/server.js
+++ app/server.js
@@ -21,9 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "../build")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"))
+})
 
 require("./routes")(app);
 
