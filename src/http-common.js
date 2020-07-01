445627349b2c74c41bee20c87aee2a2bd64aedc1
crystal <crystalmanner130@gmail.com>
2020-07-01 05:40:42 -0700

    change base url as domain url

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/",
-  //baseURL: "http://staging.inspiredcompanies.global/",
+  //baseURL: "http://localhost:8080/",
+  baseURL: "http://staging.inspiredcompanies.global/",
   // baseURL: "https://pazzin.herokuapp.com/api",
   // baseURL: "https://paziin.herokuapp.com/api",
   headers: {
