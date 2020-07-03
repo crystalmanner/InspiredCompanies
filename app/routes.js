66baf35ad0ddd425e8d6fd26570296191e5c02ef
crystalmanner <crystalmanner130@gmail.com>
2020-07-03 05:14:12 -0400

    fix internal server error

--- app/routes.js
+++ app/routes.js
@@ -5,22 +5,22 @@ module.exports = (app) => {
   router.post("/emaildata", (req, res) => {
     var fs = require("fs");
     fs.stat("./data/email.csv", function (err, stat) {
-      console.log(req.body.emailData);
+      // console.log(req.body.emailData);
       var csv = req.body.emailData + "\r\n";
       fs.appendFile("./data/email.csv", csv, function (err) {
         if (err) throw err;
-        res.send("ok");
+        res.status(400).send({ message: "ok" });
       });
     });
   });
 
   router.get("/emaildata", (req, res) => {
-    res.send("get success!")
+    res.status(400).send({ message: "get success!"})
   });
 
   router.post("/downloadcsv", function (req, res) {
     const file = `${__dirname}/data/email.csv`;
-    console.log(file);
+    // console.log(file);
     res.download(file); // Set disposition and send it.
   });
 
