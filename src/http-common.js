a8fcdec23f6f03e1654da2f42d6bb4a00c535377
crystalmanner <crystalmanner130@gmail.com>
2020-07-06 04:18:10 -0700

    implement page transition

--- src/http-common.js
+++ src/http-common.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8080/api",
-  baseURL: "http://staging.inspiredcompanies.global/api",
+  // baseURL: "http://staging.inspiredcompanies.global/api",
   // baseURL: "https://pazzin.herokuapp.com/api",
   // baseURL: "https://paziin.herokuapp.com/api",
+  baseURL: "https://inspiredcompanies.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
